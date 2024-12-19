93a95d41a4db59a9bd9410dd03a8a2226e54128f
pobertt <79865756+pobertt@users.noreply.github.com>
2024-12-19 17:03:33 +0000

    that start of adding players to a scene

--- scripts/multiplayer/multiplayer_manager.gd
+++ scripts/multiplayer/multiplayer_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var Player = preload("res://scenes/player.tscn")
+
 const port = 9999
 const server_ip = "127.0.0.1"
 var enet_peer = ENetMultiplayerPeer.new()
@@ -9,20 +11,25 @@ func host_game():
 	
 	enet_peer.create_server(port)
 	multiplayer.multiplayer_peer = enet_peer
-	
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
+	
+	add_player(1)
 	
 func join_game():
 	print("starting join")
 	
 	var client_peer = enet_peer
 	client_peer.create_client(server_ip, port)
-	
 	multiplayer.multiplayer_peer = client_peer
 
-func add_player(id: int):
-	print("adding player %s" % id)
+func add_player(peer_id: int):
+	print("adding player %s" % peer_id)
 	
-func remove_player(id: int):
-	print("removed player %s" % id)
+	var player = Player.instantiate()
+	player.player_id = peer_id
+	player.name = str(peer_id)
+	add_child(player)
+	
+func remove_player(peer_id: int):
+	print("removed player %s" % peer_id)

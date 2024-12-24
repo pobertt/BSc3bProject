9581924d0972ca383a85fc227f75ed007ff7bb83
pobertt <79865756+pobertt@users.noreply.github.com>
2024-12-24 20:42:41 +0000

    added jump and headbob

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,10 +10,11 @@ extends CharacterBody3D
 @export var walk_speed := 7.0
 @export var sprint_speed := 8.5
 
-var wish_dir := Vector3.ZERO
+const HEADBOB_MOVE_AMOUNT = 0.06
+const HEADBOB_FREQUENCY = 2.4
+var headbob_time := 0.0
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 10.0
+var wish_dir := Vector3.ZERO
 
 @export var player_id := 1:
 	set(id):
@@ -22,6 +23,10 @@ const JUMP_VELOCITY = 10.0
 # Setting up multiplayer authority, correspoding to correct peer_id.
 func _enter_tree() -> void:
 	set_multiplayer_authority(str(name).to_int())
+
+# More robust version of enabling sprint. 
+func _get_move_speed() -> float:
+	return sprint_speed if Input.is_action_pressed("sprint") else walk_speed
 
 func _ready():
 	# Checking multiplayer authority.
@@ -54,6 +59,16 @@ func _unhandled_input(event: InputEvent) -> void:
 			# Limited to looking down to feet and up to the sky. Otherwise it would rotate forever (back/frontflips)
 			camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 
+func _headbob_effect(delta: float):
+	# Gets absolute velocity. The faster we are moving, the faster our head is bobbing.
+	headbob_time += delta * self.velocity.length()
+	# Uses sine & cosine waves functions with time, to move the head up and down with time.
+	camera.transform.origin = Vector3(
+		cos(headbob_time * HEADBOB_FREQUENCY * 0.5) * HEADBOB_MOVE_AMOUNT,
+		sin(headbob_time * HEADBOB_FREQUENCY) * HEADBOB_MOVE_AMOUNT,
+		0
+	)
+
 func _process(delta: float) -> void:
 	pass
 	
@@ -61,21 +76,22 @@ func _handle_air_physics(delta: float) -> void:
 	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
 	
 func _handle_ground_physics(delta: float) -> void:
-	self.velocity.x = wish_dir.x * walk_speed
-	self.velocity.z = wish_dir.z * walk_speed
+	self.velocity.x = wish_dir.x * _get_move_speed()
+	self.velocity.z = wish_dir.z * _get_move_speed()
+	
+	_headbob_effect(delta)
 
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority(): return
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_vector("left", "right", "up", "down").normalized()
 	# Depending on which way you have your character facing, you may have to negate the input directions.
 	wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y)
 	
 	if is_on_floor():
+		# Handle jump.
+		if Input.is_action_just_pressed("jump"):
+			self.velocity.y = jump_velocity
 		_handle_ground_physics(delta)
 	else:
 		_handle_air_physics(delta)
